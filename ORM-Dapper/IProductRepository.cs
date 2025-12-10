38efd30553f9b587fa686722d691b86c841ca4c2
Kenton Knelle <kken51915@gmail.com>
2025-12-10 09:43:06 -0500

    Exercise 1 & 2 Push

---
+++ ORM-Dapper/IProductRepository.cs
@@ -0,0 +1,9 @@
+﻿namespace ORM_Dapper;
+
+public interface IProductRepository
+{
+    public IEnumerable<Product> GetAllProducts();
+    public void CreateProduct(string name, double price, int categoryID, bool onSale, int stockLevel);
+    public void UpdateProduct(string name, double price, int categoryID, bool onSale, int stockLevel, int productId);
+    public void DeleteProduct(int productId);
+}
--- ORM-Dapper/Program.cs
+++ ORM-Dapper/Program.cs
@@ -1,10 +1,41 @@
-﻿namespace ORM_Dapper
+﻿using System;
+using System.Data;
+using System.IO;
+using MySql.Data.MySqlClient;
+using Microsoft.Extensions.Configuration;
+using ORM_Dapper;
+
+//^^^^MUST HAVE USING DIRECTIVES^^^^
+
+var config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+string connString = config.GetConnectionString("DefaultConnection");
+IDbConnection conn = new MySqlConnection(connString);
+
+var departmentRepo = new DapperDepartmentRepository(conn);
+
+departmentRepo.InsertDepartment("CSharp-63");
+
+var departments = departmentRepo.GetAllDepartments();
+
+foreach (var department in departments)
 {
-    public class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
+    Console.WriteLine($"¨Name: {department.Name} | ID: {department.DepartmentID}");
+}
+
+
+var prodRepo = new ProductRepository(conn);
+
+//prodRepo.CreateProduct("Banana", 5.00, 10, false, 25);
+//prodRepo.UpdateProduct("Mario Kart 8", 60.00, 8, false, 500, 940);
+//prodRepo.DeleteProduct(940);
+
+
+var products =  prodRepo.GetAllProducts();
+
+foreach (var product in products)
+{
+    Console.WriteLine($"ID: {product.ProductID} | Name: {product.Name} | Price: {product.Price} | CategoryID: {product.CategoryID} | Sale: {product.OnSale} | Stock: {product.StockLevel}");
 }
